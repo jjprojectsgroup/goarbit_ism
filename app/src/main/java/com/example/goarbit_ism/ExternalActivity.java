8a79206bd5d7ef98b323df8bb1b6f249b2ba45e9
Junior_Rojas <jjprojects.group@gmail.com>
2022-09-20 14:32:30 -0500

    se ajusta el menu y algunos detalles

---
+++ app/src/main/java/com/example/goarbit_ism/ExternalActivity.java
@@ -0,0 +1,46 @@
+package com.example.goarbit_ism;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import com.example.goarbit_ism.ui.util.Constantes;
+
+public class ExternalActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_external);
+
+        CookieManager.getInstance();
+        webView = findViewById(R.id.webView1);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl(Constantes.url_News);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
